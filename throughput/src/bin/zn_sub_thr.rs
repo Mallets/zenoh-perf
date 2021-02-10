4eaefceb702a0b8e47fe77dd7cab833998f24da8
Luca Cominardi <luca.cominardi@gmail.com>
2021-02-10 16:47:16 +0100

    Fixed println

--- throughput/src/bin/zn_sub_thr.rs
+++ throughput/src/bin/zn_sub_thr.rs
@@ -11,6 +11,7 @@
 // Contributors:
 //   ADLINK zenoh team, <zenoh@adlink-labs.tech>
 //
+use async_std::future;
 use async_std::sync::Arc;
 use async_std::task;
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -79,14 +80,17 @@ async fn main() {
         .await
         .unwrap();
 
-    loop {
-        task::sleep(Duration::from_secs(1)).await;
-        let c = messages.swap(0, Ordering::AcqRel);
-        if c > 0 {
-            println!(
-                "zenoh-net,sub,throughput,{},{},{}",
-                opt.name, opt.payload, c
-            );
+    let name = opt.name.clone();
+    let payload = opt.payload;
+    task::spawn(async move {
+        loop {
+            task::sleep(Duration::from_secs(1)).await;
+            let c = messages.swap(0, Ordering::AcqRel);
+            if c > 0 {
+                println!("zenoh-net,sub,throughput,{},{},{}", name, payload, c);
+            }
         }
-    }
+    });
+
+    future::pending::<()>().await;
 }
