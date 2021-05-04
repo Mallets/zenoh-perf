3611b1c653fdcca142d9c7405e4285d07e1cf6ab
Luca Cominardi <luca.cominardi@gmail.com>
2021-05-04 12:55:19 +0200

    Added UDP sink

--- throughput/src/bin/s_sink_udp.rs
+++ throughput/src/bin/s_sink_udp.rs
@@ -11,12 +11,10 @@
 // Contributors:
 //   ADLINK zenoh team, <zenoh@adlink-labs.tech>
 //
-use async_std::net::{SocketAddr, TcpListener, TcpStream};
-use async_std::prelude::*;
+use async_std::net::{SocketAddr, UdpSocket};
 use async_std::sync::Arc;
 use async_std::task;
 use rand::RngCore;
-use std::convert::TryInto;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::time::Duration;
 use structopt::StructOpt;
@@ -25,51 +23,39 @@ use zenoh::net::protocol::io::{RBuf, WBuf};
 use zenoh::net::protocol::proto::{OpenSyn, SessionBody, SessionMessage};
 
 macro_rules! zsend {
-    ($msg:expr, $stream:expr) => {{
+    ($msg:expr, $socket:expr, $addr:expr) => {{
         // Create the buffer for serializing the message
         let mut wbuf = WBuf::new(32, false);
-        // Reserve 16 bits to write the length
-        assert!(wbuf.write_bytes(&[0u8, 0u8]));
         // Serialize the message
         assert!(wbuf.write_session_message(&$msg));
-        // Write the length on the first 16 bits
-        let length: u16 = wbuf.len() as u16 - 2;
-        let bits = wbuf.get_first_slice_mut(..2);
-        bits.copy_from_slice(&length.to_le_bytes());
         let mut bytes = vec![0u8; wbuf.len()];
         wbuf.copy_into_slice(&mut bytes[..]);
-
         // Send the message on the link
-        let res = $stream.write_all(&bytes).await;
+        let res = $socket.send_to(&bytes, $addr).await;
         log::trace!("Sending {:?}: {:?}", $msg, res);
-
         res
     }};
 }
 
 macro_rules! zrecv {
-    ($stream:expr, $buffer:expr) => {{
-        let _ = $stream.read_exact(&mut $buffer[0..2]).await.unwrap();
-        let length: [u8; 2] = $buffer[0..2].try_into().unwrap();
-        // Decode the total amount of bytes that we are expected to read
-        let to_read = u16::from_le_bytes(length) as usize;
-        $stream.read_exact(&mut $buffer[0..to_read]).await.unwrap();
-        let mut rbuf = RBuf::from(&$buffer[..]);
-        rbuf.read_session_message().unwrap()
+    ($socket:expr, $buffer:expr) => {{
+        let (n, addr) = $socket.recv_from(&mut $buffer).await.unwrap();
+        let mut rbuf = RBuf::from(&$buffer[..n]);
+        (rbuf.read_session_message().unwrap(), addr)
     }};
 }
 
-async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
+async fn handle_client(socket: Arc<UdpSocket>) -> Result<(), Box<dyn std::error::Error>> {
     let my_whatami = whatami::ROUTER;
     let mut my_pid = [0u8; PeerId::MAX_SIZE];
     rand::thread_rng().fill_bytes(&mut my_pid);
-    let my_pid = PeerId::new(1, my_pid);
+    let my_pid = PeerId::new(PeerId::MAX_SIZE, my_pid);
 
     // Create the reading buffer
     let mut buffer = vec![0u8; 16_000_000];
 
     // Read the InitSyn
-    let message = zrecv!(stream, buffer);
+    let (message, addr) = zrecv!(socket, buffer);
     match message.get_body() {
         SessionBody::InitSyn { .. } => {
             let whatami = my_whatami;
@@ -84,13 +70,16 @@ async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn std::error::
                 attachment,
             );
             // Send the InitAck
-            let _ = zsend!(message, stream).unwrap();
+            let _ = zsend!(message, socket, addr).unwrap();
         }
         _ => panic!(),
     }
 
     // Read the OpenSyn
-    let message = zrecv!(stream, buffer);
+    let (message, a) = zrecv!(socket, buffer);
+    if a != addr {
+        panic!("Received data from {}, expected from {}", a, addr);
+    }
     match message.get_body() {
         SessionBody::OpenSyn(OpenSyn {
             lease, initial_sn, ..
@@ -98,7 +87,7 @@ async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn std::error::
             let attachment = None;
             let message = SessionMessage::make_open_ack(*lease, *initial_sn, attachment);
             // Send the OpenAck
-            let _ = zsend!(message, stream).unwrap();
+            let _ = zsend!(message, socket, addr).unwrap();
         }
         _ => panic!(),
     }
@@ -117,38 +106,33 @@ async fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn std::error::
     });
 
     // Spawn the KeepAlive task
-    let mut c_stream = stream.clone();
+    let c_socket = socket.clone();
+    let c_addr = addr;
     task::spawn(async move {
         loop {
             task::sleep(Duration::from_secs(1)).await;
             let message = SessionMessage::make_keep_alive(None, None);
-            let _ = zsend!(message, c_stream).unwrap();
+            let _ = zsend!(message, c_socket, c_addr).unwrap();
         }
     });
 
     // Read from the socket
     loop {
-        let n = stream.read(&mut buffer).await.unwrap();
+        let (n, a) = socket.recv_from(&mut buffer).await.unwrap();
+        if a != addr {
+            panic!("Received data from {}, expected from {}", a, addr);
+        }
         let _ = counter.fetch_add(n, Ordering::Relaxed);
     }
 }
 
 async fn run(addr: SocketAddr) -> Result<(), Box<dyn std::error::Error>> {
-    let listener = TcpListener::bind(addr).await?;
-    let mut incoming = listener.incoming();
-
-    while let Some(stream) = incoming.next().await {
-        let stream = stream?;
-        task::spawn(async move {
-            let _ = handle_client(stream).await;
-        });
-    }
-
-    Ok(())
+    let socket = UdpSocket::bind(addr).await?;
+    handle_client(Arc::new(socket)).await
 }
 
 #[derive(Debug, StructOpt)]
-#[structopt(name = "s_sink_tcp")]
+#[structopt(name = "s_sink_udp")]
 struct Opt {
     #[structopt(short = "l", long = "listener")]
     listener: SocketAddr,
