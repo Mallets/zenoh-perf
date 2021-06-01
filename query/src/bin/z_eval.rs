ba6c49dd213ef1466622731e858366eb7e5c6c31
Luca Cominardi <luca.cominardi@gmail.com>
2021-06-01 08:49:46 +0200

    Added all query tests

--- query/src/bin/z_eval.rs
+++ query/src/bin/z_eval.rs
@@ -57,7 +57,7 @@ async fn main() {
     let mut get_stream = workspace.register_eval(&path.into()).await.unwrap();
     while let Some(get_request) = get_stream.next().await {
         let data = vec![0u8; opt.payload];
-        get_request.reply(path.clone(), data.into()).await;
+        get_request.reply(path.clone(), data.into());
     }
 
     get_stream.close().await.unwrap();
